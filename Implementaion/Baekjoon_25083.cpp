aefad479b714a94512570e4aa5f14147bdf1d7d0
Jung-jaewoo <wjdwodn0722@naver.com>
2022-05-01 22:57:48 +0900

    add

---
+++ Implementaion/Baekjoon_25083.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#define ll long long
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main()
+{
+    fastio;
+    cout << "         ,r'\"7\n";;
+    cout << "r`-_   ,'  ,/\n";
+    cout << " \\. \". L_r'\n";
+    cout << "   `~\\/\n";
+    cout << "      |\n";
+    cout << "      |\n";
+    return 0;
+}
